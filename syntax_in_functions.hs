6a8de2fe1e5c1e18e8c7782ce0a728699d2cd0ec
Noah Gordon <tortuga.ng@gmail.com>
2014-09-22 21:14:49 -0400

    Add file for Ch.3 and begin reading/working

---
+++ syntax_in_functions.hs
@@ -0,0 +1,77 @@
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial x = x * factorial(x - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second(_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell [x] = "The list has one element: " ++ show x  
+tell [x,y] = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = error "Empty string!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+	| bmi <= 18.5 = "You're underweight!"
+	| bmi <= 25.0 = "You're normal."
+	| bmi <= 30.0 = "You're overweight!"
+	| otherwise 	= "You're obese!"
+	where bmi = weight / height ^ 2
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis bmis = [bmi w h | (w, h) <- bmis ]
+	where bmi weight height = weight / height ^ 2
+	
+max' :: (Ord a) => a -> a -> a
+max' a b 
+	| a > b 		= a 
+	| otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+myCompare a b
+	| a > b 		= GT
+	| a < b 		= LT
+	| otherwise = EQ
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname  
+          (l:_) = lastname 
