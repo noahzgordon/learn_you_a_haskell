4bd18ac7211f71f4f26c5772734433ee3b81db0a
Noah Gordon <tortuga.ng@gmail.com>
2014-09-26 14:42:51 -0400

    More chapters! :)

--- syntax_in_functions.hs
+++ syntax_in_functions.hs
@@ -57,8 +57,7 @@ bmiTell weight height
 	where bmi = weight / height ^ 2
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis bmis = [bmi w h | (w, h) <- bmis ]
-	where bmi weight height = weight / height ^ 2
+calcBmis bmis = [bmi | (w, h) <- bmis, let bmi  = w / h ^ 2 ]
 	
 max' :: (Ord a) => a -> a -> a
 max' a b 
@@ -74,4 +73,12 @@ myCompare a b
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
     where (f:_) = firstname  
-          (l:_) = lastname 
+          (l:_) = lastname
+					
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea  
+	
+
